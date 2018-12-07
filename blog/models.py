715544d49894b9529ca76343edf0a609b4937be4
Nimaz Sheik <nimazsheik@gmail.com>
2018-12-07 17:14:22 +0530

    Created User and Post model with data

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# users - authors of the post
+# posts
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # to see sql statement run in the migration
+    # python manage.py sqlmigrate blog 0001   --- appname and migration number
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author':"Fredrick",
-        'title' : "blog post 1",
-        'content' : 'First post content',
-        'date_posted': 'August 25,2012'
-    },
-    {
-        'author':"dfer",
-        'title' : "blog post 2",
-        'content' : 'second post content',
-        'date_posted': 'August 12,2012'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     #context is a dictionary, key 'posts' will be accessible inside template
     return render(request, 'blog/home.html',context)
