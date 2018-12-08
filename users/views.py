1b9b30e50d6ad5a2c4809334a4bac7fdfc0115f0
Nimaz Sheik <nimazsheik@gmail.com>
2018-12-08 20:24:47 +0530

    Login and logout component created and functional

--- users/views.py
+++ users/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST) ### replace usercreationform with userregister form the one we made
+        form = UserRegisterForm(request.POST) # replace usercreationform with userregister form the one we made
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
-            #display the flash messages in base.html (check it)
+            messages.success(request, f'Your account has been created! You are able to login!')
+            return redirect('login')
+            # display the flash messages in base.html (check it)
     else:
         form = UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
+
+
+#django will look in default login route location
+#so need to change in settings.py
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
